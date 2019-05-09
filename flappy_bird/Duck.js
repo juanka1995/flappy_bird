c623a71629bc809fffc2d0f0c2d64da3fe0393fc
Guillergood <Guillergood@users.noreply.github.com>
2019-05-09 18:47:19 +0200

    Actualizada las animaciones
    Mejores animaciones

--- flappy_bird/Duck.js
+++ flappy_bird/Duck.js
@@ -17,10 +17,10 @@ class Duck extends THREE.Object3D {
     // Variables de las animaciones
     this.subida = 0.175;          // Incrementado o decrementado de posicion
     this.rotZ = 0;              // Vuelve a poner la rotacion en 0
-    this.duracionSubida = 500;     
+    this.duracionSubida = 250;     
     this.duracionAnimacionHoraria = 250;
     this.duracionAnimacionAntihoraria = (this.duracionAnimacionHoraria*3)/2;
-    this.limiteRotacion = 1.1;  // Limite superior e inferior de rotacion en Z
+    this.nuevaPosicionRotacion = 1.1;  // Limite superior e inferior de rotacion en Z
     this.incrementoEnY = 2;   // Siguiente posicion en la que va a estar en Y: posActual = posActual + this.incrementoEnY
 
     var posZ = 1.2;
@@ -58,14 +58,15 @@ class Duck extends THREE.Object3D {
 
     this.scale.set(0.1,0.1,0.1);
     this.position.set (0,this.y,this.z);
+    this.posicionActual = {y: 0};
+    this.nuevaPosicion = {y: 0};
     this.instanciateAnimations();
+    
+    
 
-    //this.duckBox = new Box3(new THREE.Vector3(), new THREE.Vector3());
+    this.duckBox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
   }
-  
 
-
-  
   update () {
 
     this.alaDerecha.update();
@@ -87,62 +88,68 @@ class Duck extends THREE.Object3D {
 
   // Acción de volar del pato
   fly(){
+    // Se eliminan antiguas animaciones
     TWEEN.removeAll();
-    var that= this;
-    var limite = this.y+this.incrementoEnY;
-    if(limite > this.upperBound){
-      limite = this.upperBound;
+    // Se define la nueva posicion
+    this.nuevaPosicion.y = this.y+this.incrementoEnY;
+    // Si la nueva posicion supera al limite establecido...
+    if(this.nuevaPosicion.y > this.upperBound){
+      this.nuevaPosicion.y = this.upperBound;
     }
-    this.animacionSubiendo.to({ y: limite }, that.duracionAnimacionHoraria);
+    // Se actualiza posicion actual, para el siguiente click o pulsado de barra
+    this.y = this.nuevaPosicion.y;
+    // Se define la nueva posicion en la animacion con la duracion
+    this.animacionSubiendo.to(this.nuevaPosicion, this.duracionSubida);
+    // Comienza la animacion
     this.rotacionHoraria.start();
   }
 
   startFallAnimation(){
     this.rotation.set (0,0,0);
     TWEEN.removeAll();
-    this.rotacionAntiHoraria.start();
   }
 
+  // Inicializacion de animaciones
+
   instanciateAnimations(){
+
     var that= this;
-    this.animacionBajando = new TWEEN.Tween({y: that.y})
-    .to({y: that.lowerBound})
-    .easing( TWEEN.Easing.Elastic.In)
-    .onUpdate( function ( ) {
-        that.y-=that.subida;
-        
-        if(that.outOfLowerBound()){
-          that.y = that.lowerBound;
-        }
 
-        that.position.set (0,that.y,that.z);
-        that.rotation.set (0,0,-that.limiteRotacion);
-    })
-    .onComplete( function () {console.log("COMPLETADA animacionBajando\n");})
-    .repeat(Infinity);
+    /* 
+        this.posicionActual es la variable que va a estar actualizandose en todo momento
+        .to indica a donde ir y la duracion de la animacion
+        .easing indica como es la animacion -> https://sole.github.io/tween.js/examples/03_graphs.html
+        .onUpdate define la funcion a hacer cada vez que actualiza el movimiento
+            ahi dentro se actualiza el "this.y"
+        .onComplete realiza la funcion despues de terminar la animacion
+
+        .chain en la linea 176 encadena las animaciones cuando uno termina empieza el otro...
+    */
+
+    this.animacionBajando = new TWEEN.Tween(this.posicionActual)
+                    .to( {y: that.lowerBound}, that.duracionBajada)
+                    .easing(TWEEN.Easing.Linear.None)
+                    .onUpdate(function() {
+                        that.y = that.posicionActual.y;
+                        that.position.set (0,that.posicionActual.y,that.z);
+                        console.log("animacionBajando: " + that.posicionActual.y);
+                    })
+                    .onComplete( function () {that.posicionActual.y = that.y});
 
 
-    this.animacionSubiendo = new TWEEN.Tween({y: that.y})
-    .to({y: 0}, that.duracionSubida)
-    .easing( TWEEN.Easing.Elastic.In)
-    .onUpdate( function ( ) {
-        that.y+=that.subida;
 
-        if(that.outOfUpperBound()){
-          that.y = that.upperBound;
-        }
-
-        that.position.set (0,that.y,that.z);
-        that.rotation.set (0,0,that.limiteRotacion);
-    } )
-    .onComplete( function () {
-      console.log("COMPLETADA animacionSubiendo\n");
-      that.rotation.set (0,0,0);
-    });
+    this.animacionSubiendo = new TWEEN.Tween(this.posicionActual)
+                    .to( 1, that.duracionSubida)
+                    .easing(TWEEN.Easing.Linear.None)
+                    .onUpdate(function() {
+                        that.y = that.posicionActual.y;
+                        that.position.set (0,that.posicionActual.y,that.z);
+                        console.log("animacionSubiendo: " + that.posicionActual.y);
+                    })
+                    .onComplete( function () {that.posicionActual.y = that.y});
 
     this.rotacionHoraria = new TWEEN.Tween({y: that.y})
     .to({y: 0}, that.duracionAnimacionHoraria)
-    .easing(TWEEN.Easing.Elastic.Out)
     .onUpdate( function ( ) {
         that.rotZ+=that.subida;
 
@@ -151,14 +158,10 @@ class Duck extends THREE.Object3D {
         }
 
         that.rotation.set (0,0,that.rotZ);
-    } )
-    .onComplete( function () {
-      console.log("COMPLETADA rotacionHoraria\n");
-    });
+    } );
 
     this.rotacionAntiHoraria = new TWEEN.Tween({y: that.y})
     .to({y: 0}, that.duracionAnimacionAntihoraria)
-    .easing(TWEEN.Easing.Elastic.Out)
     .onUpdate( function ( ) {
         that.rotZ-=that.subida;
 
@@ -167,10 +170,7 @@ class Duck extends THREE.Object3D {
         }
 
         that.rotation.set (0,0,that.rotZ);
-    } )
-    .onComplete( function () {
-      console.log("COMPLETADA rotacionAntiHoraria\n");
-    });
+    } );
 
 
     this.rotacionHoraria.chain(this.animacionSubiendo);
