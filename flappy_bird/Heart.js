2d624fe256d97f49842465f8dbc5c6eb00e6502e
Juan Carlos Ruiz García <jcarlosruiz95@correo.ugr.es>
2019-05-24 17:22:54 +0200

    adding bonus

--- flappy_bird/Heart.js
+++ flappy_bird/Heart.js
@@ -6,20 +6,30 @@ class Heart extends THREE.Object3D {
         this.size_ = p_size;
         this.position_ = p_position;
 
-        var x = 0, y = 0;
-        var heartShape = new THREE.Shape();
-        heartShape.moveTo( x + 5, y + 5 );
-        heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
-        heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
-        heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
-        heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
-        heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
-        heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+        var heart = new THREE.Shape();
+        var x = -2.5;
+        var y = -5;
+        heart.moveTo(x + 2.5, y + 2.5);
+        heart.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+        heart.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+        heart.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+        heart.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+        heart.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+        heart.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
 
-        var geometry = new THREE.ShapeBufferGeometry(heartShape);
-        geometry.applyMatrix(new THREE.Matrix4().makeScale(0.035, 0.035, 0.035));
+        var extrudeSettings = {
+        steps: 2,
+        depth: 2,
+        bevelEnabled: true,
+        bevelThickness: 1,
+        bevelSize: 1,
+        bevelSegments: 2,
+        };
+
+        var geometry = new THREE.ExtrudeBufferGeometry(heart, extrudeSettings);
+        geometry.applyMatrix(new THREE.Matrix4().makeScale(0.05, 0.05, 0.05));
         geometry.applyMatrix(new THREE.Matrix4().makeRotationZ(Math.PI));
-        geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0.25, 1, 0.5));
+        geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0.5, 0.5));
         var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
         var mesh = new THREE.Mesh(geometry, material) ;
         this.add(mesh);
