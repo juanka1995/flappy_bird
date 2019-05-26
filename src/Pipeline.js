c0e0e92337f5c6bae813e9325edec134f171faaf
Guillergood <Guillergood@users.noreply.github.com>
2019-05-26 16:26:14 +0200

    Compatible con movil
    Se le ha puesto basic material para mejor rendimiento y  que no se pueda reescalar el viewport para el doble toque

--- src/Pipeline.js
+++ src/Pipeline.js
@@ -10,7 +10,7 @@ class Pipeline extends THREE.Mesh {
     this.width_down_part = 1.7;
     var down_geometry = new THREE.BoxGeometry (this.width_down_part, this.size_y_down_part, 0);
     var down_texture = new THREE.TextureLoader().load('imgs/pipeline_texture.jpg');
-    var down_material = new THREE.MeshPhongMaterial ({map: down_texture});
+    var down_material = new THREE.MeshBasicMaterial ({map: down_texture});
     this.down_part = new THREE.Mesh(down_geometry, down_material);
     this.down_part.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 1.15, 0));
 
@@ -19,7 +19,7 @@ class Pipeline extends THREE.Mesh {
     this.width_up_part = 2.3;
     var up_geometry = new THREE.BoxGeometry (this.width_up_part, this.size_y_up_part, 0);
     var up_texture = new THREE.TextureLoader().load('imgs/pipeline_texture.jpg');
-    var up_material = new THREE.MeshPhongMaterial ({map: up_texture});
+    var up_material = new THREE.MeshBasicMaterial ({map: up_texture});
     this.up_part = new THREE.Mesh(up_geometry, up_material);
     this.up_part.position.y = 2.9;
 
