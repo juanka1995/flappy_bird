a5937142fd2329deae34b2b95e25911a17c33505
Juan Carlos Ruiz García <jcarlosruiz95@correo.ugr.es>
2019-05-26 02:39:20 +0200

    creating website

--- src/Pipeline.js
+++ src/Pipeline.js
@@ -9,7 +9,7 @@ class Pipeline extends THREE.Mesh {
     this.size_y_down_part = 2.3;
     this.width_down_part = 1.7;
     var down_geometry = new THREE.BoxGeometry (this.width_down_part, this.size_y_down_part, 0);
-    var down_texture = new THREE.TextureLoader().load('../imgs/pipeline_texture.jpg');
+    var down_texture = new THREE.TextureLoader().load('/imgs/pipeline_texture.jpg');
     var down_material = new THREE.MeshPhongMaterial ({map: down_texture});
     this.down_part = new THREE.Mesh(down_geometry, down_material);
     this.down_part.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 1.15, 0));
@@ -18,7 +18,7 @@ class Pipeline extends THREE.Mesh {
     this.size_y_up_part = 1.3;
     this.width_up_part = 2.3;
     var up_geometry = new THREE.BoxGeometry (this.width_up_part, this.size_y_up_part, 0);
-    var up_texture = new THREE.TextureLoader().load('../imgs/pipeline_texture.jpg');
+    var up_texture = new THREE.TextureLoader().load('/imgs/pipeline_texture.jpg');
     var up_material = new THREE.MeshPhongMaterial ({map: up_texture});
     this.up_part = new THREE.Mesh(up_geometry, up_material);
     this.up_part.position.y = 2.9;
